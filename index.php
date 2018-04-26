d23239ec5c64afd7df3a72a4bb66f673d529099b
Ibad Rahadian Saladdin <ibadsaladdin@gmail.com>
2018-04-27 03:26:21 +0700

    changes jpg to JPG

--- index.php
+++ index.php
@@ -34,7 +34,7 @@
   <!-- Navigation -->
   <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
     <div class="container">
-      <a class="navbar-brand" href="index.html">Ibad Saladdin</a>
+      <a class="navbar-brand" href="/">Ibad Saladdin</a>
       <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive"
         aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
         Menu
@@ -43,7 +43,7 @@
       <div class="collapse navbar-collapse" id="navbarResponsive">
         <ul class="navbar-nav ml-auto">
           <li class="nav-item">
-            <a class="nav-link" href="index.html">Home</a>
+            <a class="nav-link" href="/">Home</a>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="https://blog.ibad.my.id">Blog</a>
@@ -63,7 +63,7 @@
   </nav>
 
   <!-- Page Header -->
-  <header class="masthead" style="background-image: url('img/sushi-date.jpg')">
+  <header class="masthead" style="background-image: url('img/sushi-date.JPG')">
     <div class="overlay"></div>
     <div class="container">
       <div class="row">
